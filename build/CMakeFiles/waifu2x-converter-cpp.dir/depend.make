dc90a3e07863746658b0548f0e7b9f3bc4c90a70
fangzny1 <fangzny@gmail.com>
2026-07-23 13:37:53 +0800

    忽略 build 目录

--- build/CMakeFiles/waifu2x-converter-cpp.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for waifu2x-converter-cpp.
-# This may be replaced when dependencies are built.
--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /root/waifu2x-converter-cpp
-# Build directory: /root/waifu2x-converter-cpp/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
